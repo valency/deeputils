955c8359eb845adc1f83a3b2df495c7ecbf2b84a
Ye Ding <yding@deepera.com>
2017-09-06 18:25:09 +0800

    v0.9.6 [U] Version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='deeputils',
-    version='0.9.4',
+    version='0.9.6',
     packages=['deeputils'],
     url='http://open.deepera.com',
     license='CPL-3.0',
