60c3efae0291bea59002ab0877036e48b9a6442a
Ye Ding <yding@deepera.com>
2018-05-08 18:22:41 +0800

    v1.5.8

--- deeputils/common.py
+++ deeputils/common.py
@@ -4,10 +4,32 @@ from django.utils.termcolors import colorize
 
 
 def log(msg, color="green"):
+    """
+    Log with colored timestamp
+    :param msg: message
+    :param color: color, default: green
+    :return: none
+    """
     print(colorize("[" + format_datetime(datetime.now()) + "]", fg=color), msg)
 
 
+def digits(n):
+    """
+    Count the digits of a given float number
+    :param n: the float number
+    :return: number of digits
+    """
+    return len(str(n).split('.')[-1])
+
+
 def dict_search(d, k, v):
+    """
+    Search dictionary array by key and value
+    :param d: dictionary array
+    :param k: key
+    :param v: value
+    :return: the first index of the dictionary in the array with the specific key / value
+    """
     for i in range(0, len(d)):
         if d[i][k] == v:
             return i
@@ -15,6 +37,13 @@ def dict_search(d, k, v):
 
 
 def tuple_search(t, k, v):
+    """
+    Search tuple array by key and value
+    :param t: tuple array
+    :param k: key
+    :param v: value
+    :return: the first index of the tuple in the array with the specific key / value
+    """
     for e in t:
         if e[k] == v:
             return e
@@ -22,22 +51,55 @@ def tuple_search(t, k, v):
 
 
 def string_insert(str1, str2, i):
+    """
+    Insert a string in the middle of another string
+    :param str1: the original string
+    :param str2: the string to be inserted
+    :param i: the index of the insertion position
+    :return: the resulting string
+    """
     return str1[:i] + str2 + str1[i:]
 
 
 def format_datetime(t=datetime.now()):
+    """
+    Format a datetime object into yyyy-MM-dd hh:mm:ss
+    :param t: datetime object, default: now
+    :return: the formatted string
+    """
     return t.strftime('%Y-%m-%d %H:%M:%S')
 
 
 def format_date(t=datetime.now()):
+    """
+    Format a datetime object into yyyy-MM-dd
+    :param t: datetime object, default: now
+    :return: the formatted string
+    """
     return t.strftime('%Y-%m-%d')
 
 
 def format_time(t=datetime.now()):
+    """
+    Format a datetime object into hh:mm:ss
+    :param t: datetime object, default: now
+    :return: the formatted string
+    """
     return t.strftime('%H:%M:%S')
 
 
 if __name__ == "__main__":
+    a = [{
+        'a': 1,
+        'b': 2
+    }, {
+        'a': 3,
+        'b': 4
+    }]
+    log(dict_search(a, 'a', 1))
+    log(tuple_search(a, 'a', 1))
+    log(count_digits(3.1415926))
+    log(string_insert('apple', ' is strugg', 3))
     log(format_datetime())
     log(format_date())
     log(format_time())
--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 
 setup(
     name='deeputils',
-    version='1.3.2',
+    version='1.5.8',
     packages=['deeputils'],
     url='https://github.com/valency/deeputils',
-    download_url = 'https://github.com/valency/deeputils/releases/download/v1.3.2/deeputils-1.3.2.tar.gz',
+    download_url='https://github.com/valency/deeputils/releases/download/v1.5.8/deeputils-1.5.8.tar.gz',
     license='CPL-3.0',
     author='Deepera Co., Ltd.',
     author_email='yding@deepera.com',
     description='Deepera Development Utils',
-    keywords = ['utils', 'logging'],
+    keywords=['utils', 'logging'],
     install_requires=[
         'django',
         'djangorestframework'
