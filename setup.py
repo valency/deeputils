95678721733f781f7bf8d6e46b7d1f9adb4f4705
Ye Ding <yding@deepera.com>
2018-07-27 17:54:32 +0800

    v1.7.27

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-from distutils.core import setup
+import markdown2
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = '\n'.join(f.readlines())
 
 setup(
     name='deeputils',
-    version='1.7.20',
-    packages=['deeputils'],
-    url='https://github.com/valency/deeputils',
+    version='1.7.27',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/valency/deeputils/',
     license='CPL-3.0',
     author='Deepera Co., Ltd.',
     author_email='yding@deepera.com',
-    description='Deepera Development Utils',
+    description=markdown2.markdown(long_description),
     keywords=['utils', 'logging'],
     install_requires=[
         'django',
         'djangorestframework',
         'coreapi',
-        'markdown'
+        'markdown',
+        'requests'
     ],
 )
