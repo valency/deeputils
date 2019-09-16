1b44356bc41537f7b9756339f59041d6dc00c8b1
Ye Ding <guiewy@gmail.com>
2019-09-16 23:31:01 +0800

    v2.9.16

--- deeputils/common.py
+++ deeputils/common.py
@@ -15,6 +15,8 @@ class Dict2StrSafe:
         for k, v in self.__dict__.items():
             if isinstance(v, list):
                 s[k] = [json.loads(str(i)) if isinstance(i, Dict2StrSafe) else i for i in v]
+            elif isinstance(v, Dict2StrSafe):
+                s[k] = json.loads(str(v))
             else:
                 s[k] = v
         return json.dumps(s, default=lambda i: str(i))
